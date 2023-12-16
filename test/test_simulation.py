7997ee7c7584e44a392daaa9183c8fe0b929b4ff
eaalaei <147300803+eaalaei@users.noreply.github.com>
2023-12-15 22:14:48 -0800

    Update test_simulation.py

--- test/test_simulation.py
+++ test/test_simulation.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QApplication, QLineEdit, QPushButton
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 from unittest.mock import patch
-from Code.Simulation.py import TissueSimulationGUI
+from code.Simulation.py import TissueSimulationGUI
 
 class TestTissueSimulationGUI(unittest.TestCase):
     def setUp(self):
